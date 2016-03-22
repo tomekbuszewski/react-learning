e83d65eec4ec6488088739705e61ddb94d429485
Tomek Buszewski <tomek.buszewski@gmail.com>
2016-03-22 22:48:16 +0100

    firebase data

--- app/index.js
+++ app/index.js
@@ -1,47 +1,29 @@
 const React = require('react');
 const ReactDOM = require('react-dom');
+import Firebase from 'Firebase';
 const app = document.getElementById('app');
 
-class Friends {
-	constructor() {
-		this.friends = [ 'One', 'Two', 'Three' ];
-	}
-}
-
 var HelloWorld = React.createClass({
-	getInitialState: function() {
-		return { value: 'Hello!' };
-	},
-	handleChange: function(event) {
-		this.setState(
-			{ value: event.target.value }
-		);
-	},
+  getInitialState: function() {
+    return {
+      database: ''
+    };
+  },
+
+  componentDidMount: function() {
+    var serverRequest = new Firebase('https://intense-torch-9229.firebaseio.com/');
+    serverRequest.on('value', function(data) {
+      this.setState({
+        database: data.val()
+      });
+    }.bind(this));
+  },
+
 	render: function () {
-		const friends = new Friends();
-		const friendlist = friends.friends;
+    console.log(this.state.database);
 		return (
 			<div className="container">
-				<label htmlFor="name">Enter your name: </label>
-			  <input type="text" id="name" value={this.state.value} onChange={this.handleChange} />
-				<strong>{this.state.value}</strong>
-				<ShowList names={ friendlist } />
-			</div>
-		)
-	}
-});
 
-var ShowList = React.createClass({
-	render: function() {
-		const listItems = this.props.names.map((friend) => {
-			return (<li>{friend}</li>);
-		});
-		return (
-			<div>
-				<h3>List</h3>
-				<ul>
-					{ listItems }
-				</ul>
 			</div>
 		)
 	}
